c1bd4fa6ae62cb762a9dac633adf2a6d73b8b048
gotth3way <gotth3way.apis@gmail.com>
2021-03-20 21:24:09 +0100

    últimos cambios

--- src/app_server/Channel/channel.config.js
+++ src/app_server/Channel/channel.config.js
@@ -1,20 +1,40 @@
 // ########################## COMMON
-const regExEmail            = require('../_configs/constsBusiness').regExEmail;
+const regExEmail = require("../_configs/constsBusiness").regExEmail;
 // ########################## CHANNEL
-const regExChName           = /^[a-zA-Z0-9\.\-_$#@%&ñáéíóúÑÁÉÍÓÚ]{3,50}$/;
-const arrChannelTypes       = ['YOUTUBE', 'FACEBOOK', 'INSTAGRAM', 'BLOG-WEB','MARKETING-AGENCY', 'OTHER'];
-const arrChannelLanguages   = ['ES', 'EN', 'FR', 'PT', 'OTHER'];
-const arrChannelCategories  = [
-    'autos-vehicles', 'education', 'entertainment', 'film-animation', 'gaming', 'nonprofits-activism', 'howto-style', 'music', 
-    'news-politics', 'people-blogs', 'pets-animals', 'science-technology', 'sports', 'travel-events', 'comedy'
+const regExChName = /^[a-zA-Z0-9\.\-_$#@%&ñáéíóúÑÁÉÍÓÚ]{3,50}$/;
+const arrChannelTypes = [
+    "YOUTUBE",
+    "FACEBOOK",
+    "INSTAGRAM",
+    "BLOG-WEB",
+    "MARKETING-AGENCY",
+    "OTHER",
+];
+const arrChannelLanguages = ["ES", "EN", "FR", "PT", "OTHER"];
+const arrChannelCategories = [
+    "autos-vehicles",
+    "comedy",
+    "education",
+    "entertainment",
+    "film-animation",
+    "gaming",
+    "nonprofits-activism",
+    "howto-style",
+    "music",
+    "news-politics",
+    "people-blogs",
+    "pets-animals",
+    "science-technology",
+    "sports",
+    "travel-events",
 ];
 // ########################## CHANNEL
 module.exports = {
-        // COMMON
-        regExEmail,
-        // CHANNELS
-        regExChName,
-        arrChannelTypes,
-        arrChannelLanguages,
-        arrChannelCategories
-}
+    // COMMON
+    regExEmail,
+    // CHANNELS
+    regExChName,
+    arrChannelTypes,
+    arrChannelLanguages,
+    arrChannelCategories,
+};
